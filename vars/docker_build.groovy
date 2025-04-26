5218af867a327c00d51b5445f3a6b6a33b660cab
sandesh <65148993+sandeshnavsare@users.noreply.github.com>
2025-04-27 00:46:46 +0530

    Update docker_build.groovy

--- vars/docker_build.groovy
+++ vars/docker_build.groovy
@@ -1,4 +1,4 @@
 // Define function
-def call(String ProjectName, String ImageTag, String DockerHubUser){
+def call(String ProjectName, String ImageTag="latest", String DockerHubUser){
   sh "docker build -t ${DockerHubUser}/${ProjectName}:${ImageTag} ."
 }
